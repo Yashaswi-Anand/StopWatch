20499767ed5e43ed53f0b58323f7479c6a2fa41f
Yashaswi Anand <yash.kwr17@gmail.com>
2022-06-21 13:40:21 +0530

    show timer in two digit formate.

--- index.js
+++ index.js
@@ -16,15 +16,25 @@ var interval;
 
 // start button
 start[0].addEventListener('click', function() {
+    clearInterval(interval);
     interval = setInterval(function() {
+        // running millisecond timer
         mSecond.innerHTML = parseInt(mSecond.innerHTML) + 1;
+        mSecond.innerHTML = mSecond.innerHTML < 10 ? '0' + mSecond.innerHTML : mSecond.innerHTML;
+
+        // running second timer
         if (mSecond.innerHTML == 99) {
             mSecond.innerHTML = 0;
             second.innerHTML = parseInt(second.innerHTML) + 1;
+            second.innerHTML = second.innerHTML < 10 ? '0' + second.innerHTML : second.innerHTML;
         }
-        if (second.innerHTML == 60) {
+        //second.innerHTML = second.innerHTML < 10 ? '0' + second.innerHTML : second.innerHTML;
+
+        // running minute timer
+        if (second.innerHTML == 59) {
             second.innerHTML = 0;
             minute.innerHTML = parseInt(minute.innerHTML) + 1;
+            minute.innerHTML = minute.innerHTML < 10 ? '0' + minute.innerHTML : minute.innerHTML;
         }
         
     });
